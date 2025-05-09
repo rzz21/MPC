7482bd31ed2d4d39da1bd4884c845e12937c089d
rzz21 <rzz21@mails.tsinghua.edu.cn>
2025-05-09 20:18:51 +0800

    Fix solver_forward, create test_MPR.py

--- test_MPR.py
+++ test_MPR.py
@@ -1,90 +1,61 @@
-import time
-import os
 import torch
-from collections import namedtuple
-import matplotlib.pylab as plt
-import numpy
+import torch.nn as nn
+import argparse
+import time
 import math
+import os
 
 from utils import logger
+from utils import init_device, init_model, FakeLR, WarmUpCosineAnnealingLR
+from dataset import create_Env_dataloader
 from utils.statics import AverageMeter, NMSE_evaluator
 from utils.metrics import AverageMeter, para2pos, cal_metric_pos
 
-__all__ = ['MPDTester', 'MPRTester']
+def main(opt):
+    save_path = opt.root_dir + f'checkpoints/small_net/seed_{opt.seed}/test_MPD/MPD_Ns{opt.Ns}_epochs{opt.epochs}_batch_size{opt.batch_size}_lr{opt.lr}'
 
-class MPDTester:
-    def __init__(self, model, device, criterion, print_freq=20, save_path=None):
+    logger.info('=> PyTorch Version: {}'.format(torch.__version__), root=save_path)
+    logger.info(opt, root=save_path)
+
+    # Environment initialization
+    device, pin_memory, num_workers = init_device(opt.seed, opt.cpu, opt.gpu, opt.cpu_affinity, save_path)
+
+    # Create the data loader
+    train_loader, val_loader = create_Env_dataloader(path='/home/zhizhen/MPC/data/'+f'Ns_{opt.Ns}/', 
+                                          batch_size=opt.batch_size, 
+                                          num_workers=num_workers, 
+                                          device=device,
+                                          Ns_max=opt.Ns,
+                                          noise_power=opt.noise)
+    
+    # Define model
+    model_MPR = init_model(opt.pretrained, path_Num=opt.Ns, w=64, h=64, save_path=save_path)
+    model_MPR.to(device)
+
+    # Define loss function
+    criterion = nn.MSELoss().to(device)
+    
+    # MPR forward
+    results = MPRTester(model_MPR,  device, criterion, opt.Ns, save_path=save_path)(train_loader)
+    logger.info('Results: ' + ', '.join([str(item) for item in results]), root=save_path)
+
+class MPRTester:
+    def __init__(self, model, device, criterion, Ns, print_freq=20, save_path=None):
         self.model = model
         self.device = device
         self.criterion = criterion
-        self.print_freq = print_freq
-        self.save_path = save_path
-
-    def __call__(self, test_loader, verbose=True):
-        self.model.eval()
-        with torch.no_grad():
-            loss, nmse, pred_paras, nls = self._iteration(test_loader)
-            if verbose:
-                logger.info(f'=> Test nmse: {nmse:.3f}, loss: {loss:.3f}\n',
-                            root=self.save_path)
-        return loss, nmse, pred_paras, nls
-
-    def _iteration(self, data_loader):
-        iter_nmse = AverageMeter('Iter nmse')
-        iter_loss = AverageMeter('Iter loss')
-        iter_time = AverageMeter('Iter time')
-        time_tmp = time.time()
-
-        for batch_idx, (data, label_para, _, nl) in enumerate(data_loader):
-            data, label_para = data.to(self.device), label_para.to(self.device)
-            pred_para = self.model(data)
-            
-            if batch_idx == 0:
-                pred_paras = pred_para.cpu().detach().numpy()
-                nls = nl.cpu().detach().numpy()
-            else:
-                pred_paras = numpy.concatenate((pred_paras, pred_para.cpu().detach().numpy()), axis=0)
-                nls = numpy.concatenate((nls, nl.cpu().detach().numpy()), axis=0)
-            
-            loss = self.criterion(pred_para, label_para)
-
-            nmse = NMSE_evaluator(pred_para, label_para)
-            iter_nmse.update(nmse)
-            iter_loss.update(loss)
-            iter_time.update(time.time() - time_tmp)
-            time_tmp = time.time()
-
-            if (batch_idx + 1) % self.print_freq == 0:
-                logger.info(f'Test: [{batch_idx + 1}/{len(data_loader)}] '
-                            f'NMSE: {iter_nmse.avg:.3f} | '
-                            f'Total loss: {iter_loss.avg:.4f} | '
-                            f'time: {iter_time.avg:.3f}\n',
-                            root=self.save_path)
-
-        logger.info(f'=> Test NMSE: {iter_nmse.avg:.3f} | '
-                    f'loss: {iter_loss.avg:.3f} \n',
-                    root=self.save_path)
-        return iter_loss.avg, iter_nmse.avg, pred_paras, nls
-    
-class MPRTester:
-    def __init__(self, model_MPR, pred_nls, device, criterion, Ns_range, print_freq=20, save_path=None):
-        self.model_MPR = model_MPR
-        self.pred_nls = pred_nls
-        self.device = device
-        self.criterion = criterion
-        self.Ns_range = Ns_range
+        self.Ns = Ns
         self.print_freq = print_freq
         self.save_path = save_path
 
     def __call__(self, test_loader, rmse_thres=1, verbose=True):
-        for model in self.model_MPR:
-            model.eval()
+        self.model.eval()
         with torch.no_grad():
             results = self._iteration(test_loader, rmse_thres)
             if verbose:
                 logger.info('Test results: ' + ', '.join([str(item) for item in results]), root=self.save_path)
         return results
-
+    
     def _iteration(self, data_loader, rmse_thres):
         results_list = []
         catch_list = []
@@ -95,26 +66,24 @@ class MPRTester:
         iter_precision = AverageMeter("precision")
         iter_rmse = AverageMeter("rmse")
 
-        # batch_size
         for batch_idx, (data, label_para, pos, nl) in enumerate(data_loader):
+            data, label_para = data.to(self.device), label_para.to(self.device)
             _, nc, height, width = data.shape
+            pred_para = self.model(data)
+            
+            # calculate pos
             for sample_idx in range(data.shape[0]):
-                total_idx = sample_idx + batch_idx * data.shape[0]
                 nl_sample = nl[sample_idx].cpu().detach().numpy().astype(int).item()
-                data_sample = data[sample_idx].unsqueeze(0).to(self.device)
+                data_sample = data[sample_idx].unsqueeze(0)
                 pos_sample = pos[sample_idx].to(self.device)
-                label_para_sample = label_para[sample_idx].unsqueeze(0).to(self.device)
+                label_para_sample = label_para[sample_idx].unsqueeze(0)
                 label_para_sample = label_para_sample[:, :nl_sample, :]
-
-                # decide which model to use
-                Ns = self.pred_nls[total_idx]
-                model_idx = Ns - self.Ns_range[0]
-                pred_para_sample = self.model_MPR[model_idx](data_sample)
+                pred_para_sample = pred_para[sample_idx]
 
                 # calculate pos
                 pos_user_sample = pos_sample[0, :]
                 label_pos_sample = pos_sample[1: nl_sample + 1, :]
-                pred_pos_sample = para2pos(pred_para_sample.squeeze(0), pos_user_sample)
+                pred_pos_sample = para2pos(pred_para_sample, pos_user_sample)
                 chamdis, f1score, precision, error, rmse = cal_metric_pos(label_pos_sample, pred_pos_sample, threshold=1, rmse_thres=rmse_thres)
 
                 # debug 
@@ -151,7 +120,7 @@ class MPRTester:
                     plt.savefig(os.path.join(self.save_path, f'error_plot/batch{batch_idx}_si{sample_idx}_H.jpg'))
                     plt.close()
                     # print('debug')
-                
+
                 results_list.append(torch.stack((chamdis, f1score, precision, torch.sqrt(rmse.nanmean()))))
             
                 catch_list.append(not error)
@@ -160,8 +129,7 @@ class MPRTester:
                 iter_precision.update(precision)
                 iter_rmse.update(rmse)
                 rmse_avg = torch.sqrt(iter_rmse.avg) if isinstance(iter_rmse.avg, torch.Tensor) else math.sqrt(iter_rmse.avg)
-                mem = f'{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G'
-            
+
         logger.info(f"For all cases:", root=self.save_path)
         logger.info(('%15s' * 4) % ('Chamfer Dis', 'F1-score', 'Precision', 'RMSE'), root=self.save_path)
         rmse_avg = torch.sqrt(iter_rmse.avg) if isinstance(iter_rmse.avg, torch.Tensor) else math.sqrt(iter_rmse.avg)
@@ -184,4 +152,26 @@ class MPRTester:
 
         rmse_avg = torch.sqrt(iter_rmse.avg) if isinstance(iter_rmse.avg, torch.Tensor) else math.sqrt(iter_rmse.avg)
         return iter_chamdis.avg, iter_f1score.avg, iter_precision.avg, rmse_avg, detection_rate, chamdis_p, fscore_p, precision_p, rmse_p
-                
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root-dir', type=str, default='/home/zhizhen/MPC/Baseline_MPC/', help='root directory')
+    parser.add_argument('--pretrained', type=str, default='/home/zhizhen/MPC/Baseline_MPC/checkpoints/small_net/seed_2026/MPD_Ns5_epochs100_batch_size32_lr0.002/last.pth', help='Path to pre-trained model')
+    parser.add_argument('--resume', type=str, default=None, help='Path to resume training from checkpoint')
+    parser.add_argument('--seed', type=int, default=2026, help='Random seed for initialization')
+    parser.add_argument('--gpu', type=int, default=None, help='GPU ID to use')
+    parser.add_argument('--cpu', action='store_true', help='Disable GPU training')
+    parser.add_argument('--cpu-affinity', type=str, default=None, help='CPU affinity, like "0xffff"')
+    parser.add_argument('--batch-size', type=int, default=32, help='Mini-batch size')
+    parser.add_argument('--lr', type=float, default=2e-3, help='Initial learning rate')
+    parser.add_argument('--workers', type=int, default=0, help='Number of data loading workers')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train')
+    parser.add_argument('--rmse-thres', type=float, default=1, help='rmse threshold')
+    parser.add_argument('--noise', type=float, default=0, help='power of noise' )
+    parser.add_argument('--Ns', type=int, default=5, help='number of paths')
+    opt = parser.parse_args()
+    return opt
+
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
